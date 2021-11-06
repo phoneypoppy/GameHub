76daa84c1e82a202caa9eab59e55a2aeb406bf1a
jacoblim99 <jacob.lim.2020@smu.edu.sg>
2021-11-07 01:22:43 +0800

    alot of changes
    Coded the whole website to be functioning

--- src/views/Blogs.vue
+++ src/views/Blogs.vue
@@ -1,89 +1,84 @@
 <template>
-    <div class="blog-card-wrap">
-        <div class="blog-cards container">
-            <div class="toggle-edit">
-                <span>Toggle Editing Post</span>
-                <input type="checkbox" v-model="editPost">
-            </div>
-            <BlogCard :post="post" v-for="(post, index) in sampleBlogCards" :key="index" />
-        </div>
-    </div> 
-
+  <div class="blog-card-wrap">
+    <div class="blog-cards container">
+      <div v-if="profileAdmin" class="toggle-edit">
+        <span>Toggle Editing Post</span>
+        <input type="checkbox" v-model="editPost" />
+      </div>
+      <BlogCard :post="post" v-for="(post, index) in blogPosts" :key="index" />
+    </div>
+  </div>
 </template>
 
 <script>
 import BlogCard from "../components/BlogCard";
 export default {
-    name: "blogs",
-    components: { BlogCard },
-    computed:{
-        sampleBlogCards(){
-            return this.$store.state.sampleBlogCards;
-            },
-    editPost:{
-        get(){
-            return this.$store.state.editPost
-            },
-        set(payload){
-            this.$store.commit("toggleEditPost", payload);
-            },
-        }
+  name: "blogs",
+  components: { BlogCard },
+  computed: {
+    blogPosts() {
+      return this.$store.state.blogPosts;
     },
-    beforeDestroy() {
-        this.$store.commit("toggleEditPost", false);
-    }
-}
+    editPost: {
+      get() {
+        return this.$store.state.editPost;
+      },
+      set(payload) {
+        this.$store.commit("toggleEditPost", payload);
+      },
+    },
+  },
+  beforeDestroy() {
+    this.$store.commit("toggleEditPost", false);
+  },
+};
 </script>
 
-<style lang= "scss" scoped>
-
+<style lang="scss" scoped>
 .blog-cards {
-    position: relative;
+  position: relative;
 
-    .toggle-edit {
-        display: flex;
-        align-items: center;
-        position: absolute;
-        top: -70px;
-        right: 0;
+  .toggle-edit {
+    display: flex;
+    align-items: center;
+    position: absolute;
+    top: -70px;
+    right: 0;
 
-        span {
-        margin-right: 16px;
-        }
-
-        input[type="checkbox"] {
-        position: relative;
-        border: none;
-        -webkit-appearance: none;
-        background: #fff;
-        outline: none;
-        width: 80px;
-        height: 30px;
-        border-radius: 20px;
-        box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
-        }
-
-        input[type="checkbox"]:before {
-        content: "";
-        position: absolute;
-        width: 30px;
-        height: 30px;
-        border-radius: 20px;
-        top: 0;
-        left: 0;
-        background: #303030;
-        transform: scale(1.1);
-        transition: 750ms ease all;
-        box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
-        }
-
-        input:checked[type="checkbox"]:before {
-        background: #fff;
-        left: 52px;
-        }
+    span {
+      margin-right: 16px;
     }
+
+    input[type="checkbox"] {
+      position: relative;
+      border: none;
+      -webkit-appearance: none;
+      background: #fff;
+      outline: none;
+      width: 80px;
+      height: 30px;
+      border-radius: 20px;
+      box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+    }
+
+    input[type="checkbox"]:before {
+      content: "";
+      position: absolute;
+      width: 30px;
+      height: 30px;
+      border-radius: 20px;
+      top: 0;
+      left: 0;
+      background: #303030;
+      transform: scale(1.1);
+      transition: 750ms ease all;
+      box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+    }
+
+    input:checked[type="checkbox"]:before {
+      background: #fff;
+      left: 52px;
+    }
+  }
 }
-
-
-
 </style>
