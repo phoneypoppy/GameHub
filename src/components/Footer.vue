d0afb3fe6444cb99cf9cec821e35e9533b2df51a
jacoblim99 <jacob.lim.2020@smu.edu.sg>
2021-11-11 10:58:00 +0800

    Update Footer.vue
    change game blog to gamehub

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="left">
                 <div class="col-1">
-                    <router-link class="header" :to="{name:'Home'}">GameBlog</router-link>
+                    <router-link class="header" :to="{name:'Home'}">GameHub</router-link>
                     <!-- <ul>
                         <li> 
                             <a href="#"><youTube class="svg-icon"/></a>
