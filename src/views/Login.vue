0b8db1b54df82d7e2bc67bc11a9c171110ac5530
minthukha99 <minthukha99@gmail.com>
2021-11-11 14:48:16 +0800

    back button in login page,need to edit the design

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="form-wrap">
         <form class="login">
+            <router-link :to="{ name: 'Home' }">Back</router-link>
             <p class="login-register">
                 Don't have an account?
                 <router-link class="router-link" :to="{name: 'Register' }">Register</router-link>
