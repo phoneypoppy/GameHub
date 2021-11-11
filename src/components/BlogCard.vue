524ed1ce840a48625a892fb90c3986c374d717e6
phoeni <phoenikelly@gmail.com>
2021-11-11 15:57:12 +0800

    fixed posts page

--- src/components/BlogCard.vue
+++ src/components/BlogCard.vue
@@ -21,29 +21,29 @@
 
 <script>
 import Arrow from "../assets/Icons/arrow-right-light.svg";
-import Edit from "../assets/Icons/edit-regular.svg";
-import Delete from "../assets/Icons/trash-regular.svg";
+// import Edit from "../assets/Icons/edit-regular.svg";
+// import Delete from "../assets/Icons/trash-regular.svg";
 export default {
   name: "blogCard",
   props: ["post"],
   components: {
     Arrow,
-    Edit,
-    Delete,
+    // Edit,
+    // Delete,
   },
-  methods: {
-    deletePost() {
-      this.$store.dispatch("deletePost", this.post.blogID);
-    },
-    editBlog() {
-      this.$router.push({ name: "EditBlog", params: { blogid: this.post.blogID } });
-    },
-  },
-  computed: {
-    editPost() {
-      return this.$store.state.editPost;
-    },
-  },
+  // methods: {
+  //   deletePost() {
+  //     this.$store.dispatch("deletePost", this.post.blogID);
+  //   },
+  //   editBlog() {
+  //     this.$router.push({ name: "EditBlog", params: { blogid: this.post.blogID } });
+  //   },
+  // },
+  // computed: {
+  //   editPost() {
+  //     return this.$store.state.editPost;
+  //   },
+  // },
 };
 </script>
 
