2f7129f9b1d3519161ffe71de033fd0070e403fb
minthukha99 <minthukha99@gmail.com>
2021-11-11 14:37:35 +0800

    footer posts to games

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -22,9 +22,9 @@
                 <div class="col-2">
                     <ul>
                         <router-link class="link" :to="{name : 'Home' }">Home</router-link> 
-                        <router-link class="link" :to="{name : 'Posts' }">Posts </router-link> 
+                        <router-link class="link" :to="{name : 'Posts' }">Games </router-link> 
                         <router-link v-if="user" class="link" to="#">Create Post</router-link> 
-                        <router-link v-if="!user" class="link" :to="{name : 'Login' }">Login / register </router-link>
+                        <router-link v-if="!user" class="link" :to="{name : 'Login' }">Login </router-link>
 
                     </ul> 
                 </div>
